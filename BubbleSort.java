1762e717d8141fcc532f3c260a74f07f83d9a5a4
Gayatri2131 <byalegayatri55@gmail.com>
2026-08-21 20:28:44 +0530

    Bubble Sort program

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+//Bubble Sort
+class BubbleSort {
+    public static void printArray(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+    int arr[] = {7,8,3,1,2};
+    for (int i = 0; i < arr.length-1; i++){
+        for (int j = 0; j < arr.length-i-1; j++){
+            if (arr[j] > arr[j+1]){
+                // swap arr[j+1] and arr[i]
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+}
+}
+ System.out.println("Sorted array");
+printArray(arr);
+}
+}
